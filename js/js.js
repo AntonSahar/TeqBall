cf1aecc30d3916a35e67e3349f70f2a6f59bba57
AntonSahar <56511744+AntonSahar@users.noreply.github.com>
2020-04-20 22:17:59 +0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,25 @@
+$('.carousel').carousel({
+  interval: 16000
+});
+// $('#carouselExampleControls').on('slid.bs.carousel', function () {
+//
+//        //alert('Отображено
+//             $(".carousel-item").css('opacity','100%');
+//       $(".carousel-item.active").css('opacity','10%');
+//
+// //$(".carousel-item").animate({'opacity':'10%'},400);
+//       //document.getElementsByClassName('carousel-item').style.
+//   // do something…
+// })
+// $('#carouselExampleControls').on('slide.bs.carousel', function () {
+//
+//        //alert('Отображено
+//       $(".carousel-item.active").css('opacity','10%');
+//
+// //$(".carousel-item").animate({'opacity':'10%'},400);
+//       //document.getElementsByClassName('carousel-item').style.
+//   // do something…
+// })
+// $('#carouselExampleControls').carousel('next',function(){
+//   $(".carousel-item.active").css('opacity','100%');
+// })
